f11a98fbc355786a564f044229ecc926909f9570
Shubham Tanwar <shubham.tanwar23@gmail.com>
2020-04-05 17:08:27 +0530

    Remove title from AppBar

--- lib/screens/Groups/Components/GroupPageTopbar/GroupPageTopbar.dart
+++ lib/screens/Groups/Components/GroupPageTopbar/GroupPageTopbar.dart
@@ -6,45 +6,14 @@ class GroupPageTopbar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-        backgroundColor: Colors.white,
-        title: Container(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  Column(
-                    children: <Widget>[
-                      Icon(
-                        Icons.account_circle,
-                        size: 35.0,
-                        color: Theme.of(context).primaryColor,
-                      ),
-                    ],
-                  ),
-                  Column(
-                    children: <Widget>[
-                      Container(
-                        padding: EdgeInsets.only(left: 8.0),
-                        child: Text('Vivek Rajoriya',
-                            style: Theme.of(context).textTheme.title),
-                      )
-                    ],
-                  ),
-                ],
-              ),
-              Column(
-                children: <Widget>[
-                  Icon(
-                    Icons.notifications,
-                    size: 35.0,
-                    color: Theme.of(context).primaryColor,
-                  ),
-                ],
-              )
-            ],
-          ),
-        ));
+      backgroundColor: Colors.white,
+      actions: <Widget>[
+        Icon(
+          Icons.notifications,
+          size: 35.0,
+          color: Theme.of(context).primaryColor,
+        ),
+      ],
+    );
   }
 }
