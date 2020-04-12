5ab9c9f8d1e37fcbcf5647e6f967096d384e7f75
sanju-developer <vivekrajoriya106@gmail.com>
2020-04-12 12:18:20 +0530

    add the poisition of sidebar in main page

--- lib/screens/signup/signup_with_phone/otp_verification.dart
+++ lib/screens/signup/signup_with_phone/otp_verification.dart
@@ -1,3 +1,4 @@
+import 'package:day2day/screens/groups/groups.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
@@ -184,6 +185,10 @@ class _OTPVerificationPageState extends State<OTPVerificationPage> {
                             duration: Duration(seconds: 2),
                           ));
                         });
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => GroupsPage()));
                       }
                     },
                     child: Center(
