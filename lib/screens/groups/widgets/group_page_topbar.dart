584db46664d8eaedd372831b7a33a5f05610adef
Shubham Tanwar <shubham.tanwar23@gmail.com>
2020-04-12 19:43:45 +0530

    Fix drawer issue

--- lib/screens/groups/widgets/group_page_topbar.dart
+++ lib/screens/groups/widgets/group_page_topbar.dart
@@ -1,5 +1,3 @@
-import 'package:day2day/widgets/drawer/drawer.dart';
-import 'package:day2day/widgets/size_route/size_route.dart';
 import 'package:day2day/screens/notification/notification.dart';
 
 import 'package:flutter/material.dart';
@@ -12,7 +10,6 @@ class GroupPageTopbar extends StatelessWidget implements PreferredSizeWidget {
     return AppBar(
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       actions: <Widget>[
-        AnimatedDrawer(),
         Container(
           padding: EdgeInsets.only(right: 10.0),
           child: Column(
