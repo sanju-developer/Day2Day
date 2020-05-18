6abb779eab064dc61488f14e3074da656e48a88e
Shubham Tanwar <shubham.tanwar23@gmail.com>
2020-05-18 19:26:38 +0530

    Create constants related to API

--- lib/api/client.dart
+++ lib/api/client.dart
@@ -1,8 +1,9 @@
 import 'package:chopper/chopper.dart';
+import 'package:day2day/api/constants.dart';
 import 'package:day2day/api/login/login_service.dart';
 
 ChopperClient apiClient = ChopperClient(
-  baseUrl: "http://10.0.2.2:8000",
+  baseUrl: BASE_URL,
   converter: JsonConverter(),
   services: [
     LoginService.create(),
--- lib/api/login/login_service.dart
+++ lib/api/login/login_service.dart
@@ -1,9 +1,10 @@
 import 'dart:async';
 import 'package:chopper/chopper.dart';
+import 'package:day2day/api/constants.dart';
 
 part 'login_service.chopper.dart';
 
-@ChopperApi(baseUrl: '/api/v1/users/login/')
+@ChopperApi(baseUrl: '$API_URL$LOGIN_ENDPOINT')
 abstract class LoginService extends ChopperService {
   static LoginService create([ChopperClient client]) => _$LoginService(client);
 
